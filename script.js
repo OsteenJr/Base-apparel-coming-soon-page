034dee9addced579217e3064483f0af4693cc2ea
Osteenjr <osteenonyeke@gmail.com>
2025-01-23 01:05:54 +0100

    Second Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+ScrollReveal().reveal(".logo", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+    delay: 100,    
+});
+
+ScrollReveal().reveal(".container-text", {
+    origin: "bottom",
+    distance: "30px",
+    opacity: 0,
+    duration: 1000,
+    delay: 200,    
+});
+ScrollReveal().reveal(".container-image", {
+    origin: "right",
+    distance: "40px",
+    opacity: 0,
+    duration: 1000,
+    delay: 200,    
+});
+
